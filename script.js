3bcd3ae3efc9bd5a0d8640e1b483119c13871a43
sxmber <williamsmd002@gmail.com>
2023-02-27 18:13:50 -0700

    added computing logic

--- script.js
+++ script.js
@@ -2,41 +2,44 @@ const output = document.getElementById('output');
 const numbers = document.getElementsByClassName('number');
 const operators = document.getElementsByClassName('operators');
 const buttons = document.querySelectorAll('div#calculator-grid button')
-let display = ""
-let firstNum = ""
+let input = ""
 let array = [];
-let sum;
 let currentOperation = null;
-console.log(buttons)
-
-function inputOperand(operand){
-    array.push(operand);
-}
 
 function buttonListener() {
-    for (i=0;i<buttons.length;i++){
+    for (i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', (e) => {
-            if(e.target.className == "number"){
-                display += e.target.textContent
-                updateDisplay(display);
-            }
-            else if(e.target.className == "operators"){
-                currentOperation = e.target.textContent;
-                inputOperand(output.textContent);
-                display = "";
+            if (e.target.className == "number") {
+                input += e.target.textContent;
+                updateDisplay(input);
 
             }
-            else if(e.target.className == "span-two equals"){
-                inputOperand(output.textContent);
-                updateDisplay(operate(currentOperation, parseInt(array[0]), parseInt(array[1])))
+            else if (e.target.className == "operators") {
+                inputOperand(input);
+                inputOperand(e.target.textContent);
+                input = "";
+
+
+            }
+            else if (e.target.className == "span-two equals") {
+                inputOperand(input);
+                updateDisplay(operate(array));
+            }
+            else if (e.target.className == "del") {
+                input = input.slice(0, -1);
+                output.textContent = input;
             }
         })
     }
 }
 buttonListener()
-function updateDisplay(display){
-    //take button text content and update output text content
-   output.textContent = display;
+
+function inputOperand(operand) {
+    array.push(operand);
+}
+
+function updateDisplay(display) {
+    output.textContent = display;
 }
 
 function add(num1, num2) {
@@ -55,21 +58,25 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate(operator, num1, num2) {
-    switch (operator) {
-        case '+':
-            return add(num1, num2);
-            break;
-        case '-':
-            return subtract(num1, num2);
-            break;
-        case '*':
-            return multiply(num1, num2);
-            break;
-        case '÷':
-            return divide(num1, num2);
-            break;
-        default:
-            return "Not a valid operator";
+function operate(array) {
+    let result = parseFloat(array[0]);
+    console.log(result)
+    for (i = 1; i < array.length; i = i + 2) {
+        switch (array[i]) {
+            case '+':
+                result += parseFloat(array[i + 1]);
+                break;
+            case '-':
+                result += parseFloat(array[i + 1]);
+                break;
+            case '*':
+                result *= parseFloat(array[i + 1]);
+                break;
+            case '÷':
+                result /= parseFloat(array[i + 1]);
+                break;
+        }
     }
+    return result;
+
 }
